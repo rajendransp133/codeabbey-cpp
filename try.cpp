343498cf6544ea1f356700fdab18d11659dcf2b2
Rajendran Sp <rajendransp133@gmail.com>
2022-09-21 18:56:53 +0530

    Create try.cpp

---
+++ try.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        double x,j=0,sum=0;
+        while(cin>>x){
+            if(x==0){break;}
+            else 
+            {
+            sum+=x;
+            j++;
+            }
+        }
+    cout<<round(sum/j)<<" ";
+    }
+}
